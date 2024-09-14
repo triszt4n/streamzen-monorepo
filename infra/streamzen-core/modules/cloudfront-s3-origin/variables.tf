7a185f008df3ed5060d27190fd613bef275b8e03
Trisztán Piller <piller.trisztan@simonyi.bme.hu>
2024-09-14 03:39:26 +0200

    add frontend resources

---
+++ infra/streamzen-core/modules/cloudfront-s3-origin/variables.tf
@@ -0,0 +1,23 @@
+variable "environment" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string  
+}
+
+variable "web_acl_arn" {
+  type = string
+}
+
+variable "alb_domain_name" {
+  type = string
+}
+
+variable "alb_api_key" {
+  type = string
+}
+
+variable "acm_cert_arn" {
+  type = string
+}
--- infra/streamzen-core/variables.tf
+++ infra/streamzen-core/variables.tf
@@ -7,3 +7,13 @@ variable "region" {
   description = "The region to deploy the infrastructure to"
   type        = string
 }
+
+variable "domain_name" {
+  description = "The domain name to use for the CloudFront distribution"
+  type        = string
+}
+
+variable "api_domain_name" {
+  description = "The domain name to use for the ALB"
+  type        = string
+}
