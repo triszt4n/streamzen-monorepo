8fc95713a43b061446a4b6bbf6f71b1f4f9cb21e
Trisztán Piller <piller.trisztan@simonyi.bme.hu>
2025-03-26 17:06:14 +0100

    renaming

--- infra/streamzen-core/modules/cloudfront-s3-origin/functions.tf
+++ infra/streamzen-core/modules/cloudfront-s3-origin/functions.tf
@@ -3,11 +3,17 @@ resource "aws_cloudfront_function" "request_function" {
   runtime = "cloudfront-js-2.0"
   publish = true
   code    = file("${path.module}/function-codes/frontend-request-default.js")
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_cloudfront_function" "url_rewrite_function" {
-  name    = "streamzen-media-assets-function-${var.environment}"
+  name    = "streamzen-url-rewrite-function-${var.environment}"
   runtime = "cloudfront-js-2.0"
   publish = true
   code    = file("${path.module}/function-codes/url-rewrite.js")
+  lifecycle {
+    create_before_destroy = true
+  }
 }
